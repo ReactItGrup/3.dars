2ff4a83d590ae785efd3de27911e84263604b8d4
ReactItGrup <140500952+ReactItGrup@users.noreply.github.com>
2023-08-08 14:36:17 +0500

    Add files via upload

---
+++ 4-dars/6. Array/script.js
@@ -0,0 +1,48 @@
+// const mehmon1 = "Doniyor"
+// const mehmon3 = "Azizbek"
+// const mehmon4 = "Yodgor"
+// const mehmon5 = "Otabek"
+
+// Bu yol ancha no-qulay buni qisqartirib yozish uchun array qilib yozishiimiz mumkin
+
+// array => massive
+
+let mehmonlar = ["Doniyor", "Azizbek", "Yodgor", "Otabek"];
+
+console.log(mehmonlar);
+
+// const qiymatini o'zgartirish iloji yo'q narsa lekin array bilan bu ishni qilish mumkin
+
+// mehmonlar[0] = "Asilbek"
+// console.log(mehmonlar[0]);
+// ^^^^^^^^
+// ||||||||
+// Bu yo'l bilan O'zgaruvchi qiymatini o'zgartirishimiz mumkin
+
+let numbers = [1, 2, 3, 4, 5, 131];
+
+console.log(numbers);
+
+// property => ' elementlarni qanday ish bajarishi '
+
+// let result = numbers.length
+
+// console.log(result);
+
+// methods
+
+// let result = numbers.indexOf(4)
+
+// agar 2ta o'zgaruvchini bir-biriga qo'shish kerak bo'lsa
+// concat metodi dan foydalanamiz
+
+// let result = numbers.concat(mehmonlar)
+// console.log(result);
+
+// mehmonlar.push("Ziyodulla")
+// push => Qiymatimiz oxiriga ma'lumot qo'shish
+
+// mehmonlar.pop()
+// pop => Qiymatimiz oxiridan ma'lumot olib tashlash
+
+// console.log(mehmonlar);
---
+++ 4-dars/7. Null & Undefined/app.js
@@ -0,0 +1,15 @@
+// Null & Undefined
+
+let age
+
+// O'zgaruvchi mavjud lekin qiymat mavjud emas => Undefined
+
+console.log(age, age + 3,);
+
+let age2 = null 
+
+// null => hech qanday qiymatga ega emas va matematik qiymati 0 ga teng
+
+// null bizga input yasayotganimzda reset uchun kerak bo'ladi
+
+console.log(age2, age2 + 3);
